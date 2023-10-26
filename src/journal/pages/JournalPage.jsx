dab8b8190096d7e879a712f294b31e0980658d44
Andres <andres.torrez.work@gmail.com>
2023-10-26 16:10:59 -0400

    013 terminado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,18 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    
+    <JournalLayout>
+
+      <Typography>Excepteur laboris consequat consequat eu ullamco. Qui esse consequat incididunt sunt Lorem amet incididunt elit ex aliquip sint excepteur. Ex occaecat velit voluptate velit aliquip ex commodo minim ea. Labore deserunt cupidatat sunt mollit culpa sunt culpa culpa. In laborum aliquip aliquip eiusmod id mollit cillum aute ad nostrud sunt anim laborum. Proident adipisicing incididunt cupidatat ullamco mollit. Exercitation sint dolore deserunt sit sint est.</Typography>
+
+      {/* NothinSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
 
   )
 }
