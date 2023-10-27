15f6ddbff1ab2477c4739fe603930b44a9be0879
Andres <andres.torrez.work@gmail.com>
2023-10-26 23:35:17 -0400

    014 terminado

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,23 +1,24 @@
-import { Box } from '@mui/material'; // revisar esto parece que es /system
-import { NavBar } from "../components";
+import { Box, Toolbar } from '@mui/material'; // revisar esto parece que es /system
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
 
       {/* Navbar drawerWidth*/}
-
       <NavBar drawerWidth={ drawerWidth } />
 
       {/* Sidebar drawerWidth*/}
+      <SideBar drawerWidth={ drawerWidth } />
 
       <Box
         component='main'
         sx={{ flexGrow: 1, p: 3 }}
       >
         {/* Toolbar */}
+        <Toolbar/>
 
         { children }
 
