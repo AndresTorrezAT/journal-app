3f3e3c2ddd342b8bf1b250a28d4df4c85fdd620a
Andres <andres.torrez.work@gmail.com>
2023-10-28 00:28:42 -0400

    empezando redux

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,8 @@
-import { Typography } from '@mui/material';
+
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 
 export const JournalPage = () => {
@@ -7,10 +10,28 @@ export const JournalPage = () => {
     
     <JournalLayout>
 
-      <Typography>Excepteur laboris consequat consequat eu ullamco. Qui esse consequat incididunt sunt Lorem amet incididunt elit ex aliquip sint excepteur. Ex occaecat velit voluptate velit aliquip ex commodo minim ea. Labore deserunt cupidatat sunt mollit culpa sunt culpa culpa. In laborum aliquip aliquip eiusmod id mollit cillum aute ad nostrud sunt anim laborum. Proident adipisicing incididunt cupidatat ullamco mollit. Exercitation sint dolore deserunt sit sint est.</Typography>
+      {/* <Typography>Excepteur laboris consequat consequat eu ullamco. Qui esse consequat incididunt sunt Lorem amet incididunt elit ex aliquip sint excepteur. Ex occaecat velit voluptate velit aliquip ex commodo minim ea. Labore deserunt cupidatat sunt mollit culpa sunt culpa culpa. In laborum aliquip aliquip eiusmod id mollit cillum aute ad nostrud sunt anim laborum. Proident adipisicing incididunt cupidatat ullamco mollit. Exercitation sint dolore deserunt sit sint est.</Typography> */}
 
       {/* NothinSelected */}
+      <NothingSelectedView/>
+
       {/* NoteView */}
+      {/* <NoteView/> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+
+      </IconButton>
 
     </JournalLayout>
 
